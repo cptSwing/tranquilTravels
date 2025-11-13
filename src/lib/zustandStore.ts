a0f42a7f747ccfc7d78b3d5f976d9728f7e58be4
cptSwing <jens@jbrandenburg.de>
2025-11-14 00:04:10 +0100

    fleshed out Zustand store

--- src/lib/zustandStore.ts
+++ src/lib/zustandStore.ts
@@ -1,14 +1,29 @@
 import { create } from 'zustand';
 import { immer } from 'zustand/middleware/immer';
 import { ZustandStore } from '../types/types';
+import config from '../config/config.json';
 
 export const useZustandStore = create<ZustandStore>()(
     immer((set, get) => ({
-        values: { countries: [] },
+        values: {
+            countries: [],
+            travelDates: {
+                from: config.date.from,
+                to: config.date.to,
+            },
+        },
+
         methods: {
             store_setCountries(countries) {
                 set((draftStore) => {
                     draftStore.values.countries = countries;
+                });
+            },
+
+            store_setTravelDates(travelDates) {
+                console.log('%c[zustandStore]', 'color: #b7b528', `travelDates :`, travelDates);
+                set((draftStore) => {
+                    draftStore.values.travelDates = { ...draftStore.values.travelDates, ...travelDates };
                 });
             },
         },
--- src/types/types.ts
+++ src/types/types.ts
@@ -1,6 +1,15 @@
 import { components } from './openHolidaysSchema';
 
 export interface ZustandStore {
-    values: { countries: components['schemas']['CountryReference']['isoCode'][] };
-    methods: { store_setCountries: (countries: components['schemas']['CountryReference']['isoCode'][]) => void };
+    values: {
+        countries: components['schemas']['CountryReference']['isoCode'][];
+        travelDates: {
+            from: string; // 2025-01-01 (Date.toLocaleDateString('en-CA'))
+            to: string; // 2025-12-31
+        };
+    };
+    methods: {
+        store_setCountries: (countries: ZustandStore['values']['countries']) => void;
+        store_setTravelDates: (travelDates: Partial<ZustandStore['values']['travelDates']>) => void;
+    };
 }
