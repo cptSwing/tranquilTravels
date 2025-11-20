10026383805e2d07107d3c997c62264a731a4e4e
cptSwing <jens@jbrandenburg.de>
2025-11-20 02:11:30 +0100

    renamed ResultsByMonth to Calendar

--- src/App.tsx
+++ src/App.tsx
@@ -2,24 +2,24 @@ import { QueryClientProvider } from '@tanstack/react-query';
 import { queryClientTanstack } from './lib/api';
 import ChooseCountries from './components/ChooseCountries';
 import ChooseDateRange from './components/ChooseDateRange';
-import ResultsByMonth from './components/ResultsByMonth';
+import Calendar from './components/Calendar';
 import Header from './components/Header';
 import RangeDescription from './components/RangeDescription';
 
 const App = () => (
-    <div className="bg-theme-background h-dvh w-dvw overflow-x-hidden [--header-height:--spacing(24)]">
+    <div className="bg-theme-background h-dvh w-dvw overflow-x-hidden text-sm [--header-height:--spacing(24)]">
         <QueryClientProvider client={queryClientTanstack}>
             <Header />
 
             <main className="mx-auto flex w-[90%] flex-col items-center justify-start gap-y-2 pt-2 md:w-4/5">
-                <div className="level-1 mx-auto flex w-fit justify-center gap-x-4 p-2">
+                <div className="level-1 mx-auto flex w-full justify-around gap-3 px-3 py-2.5">
                     <ChooseDateRange />
                     <ChooseCountries />
                 </div>
 
                 <RangeDescription />
 
-                <ResultsByMonth />
+                <Calendar />
 
                 {/* <div className="bg-neutral-500 p-2">
                     <DebugHolidayView />
---
+++ src/components/Calendar.tsx
@@ -0,0 +1,207 @@
+import { useZustandStore } from '../lib/zustandStore';
+import { MONTH_NAMES, WEEKDAY_NAMES } from '../types/consts';
+import { DateRange, DayCellData, MonthData } from '../types/types';
+import { isInRange } from '../lib/handleDates';
+import { $api } from '../lib/api';
+import config from '../config/config.json';
+import useProcessHolidayResponse from '../hooks/useProcessHolidayResponse';
+import isDefined from '../lib/isDefined';
+import { classNames } from 'cpts-javascript-utilities';
+import useCreateCalendarMonths from '../hooks/useCreateCalendarMonths';
+import DisplayError from './DisplayError';
+
+const debug = false;
+const store_setRangeDescription = useZustandStore.getState().methods.store_setRangeDescription;
+
+const Calendar = () => {
+    const selectedCountries = useZustandStore((store) => store.values.countries);
+    const { from, to } = useZustandStore((store) => store.values.dateRange);
+
+    const {
+        data,
+        error,
+        isLoading: _isLoading,
+    } = $api.useQuery(
+        'get',
+        '/SchoolHolidays',
+        {
+            params: {
+                query: {
+                    countryIsoCode: selectedCountries[0],
+                    validFrom: from.dateString, // TODO should be from beginning of month (or rather, beginning of calenderview)
+                    validTo: to.dateString, // TODO should be to end of month (or rather, end of calenderview)
+                    languageIsoCode: config.language,
+                },
+            },
+        },
+        {
+            enabled: isDefined(selectedCountries[0]),
+        },
+    );
+    const { blockedRanges } = useProcessHolidayResponse(data);
+    const monthsData = useCreateCalendarMonths({ from, to });
+
+    if (error) return <DisplayError error={error} />;
+    // if (isLoading) return <DisplayLoading />;
+    if (!monthsData) return;
+
+    return (
+        <div className="level-1 pointer-events-none mb-4 grid w-full grid-cols-1 gap-4 px-3 pt-1.5 pb-2.5 [--calendar-grid-cell-height:--spacing(8)] [--calendar-grid-cell-width:--spacing(auto)] sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+            {monthsData?.map((monthData, idx) => (
+                <CalendarMonth
+                    key={from.dateString + to.dateString + idx}
+                    monthData={monthData}
+                    userRange={{ startDate: from.dateString, endDate: to.dateString }}
+                    blockedRanges={blockedRanges}
+                />
+            ))}
+        </div>
+    );
+};
+
+export default Calendar;
+
+const CalendarMonth = ({ monthData, userRange, blockedRanges }: { monthData: MonthData; userRange: DateRange; blockedRanges?: DateRange[] | undefined }) => {
+    const { monthIndex, year, cells } = monthData;
+
+    return (
+        <div>
+            <span className="text-theme-cta-foreground mb-0.5 inline-block pl-1.5 text-left font-serif text-base">
+                {MONTH_NAMES[monthIndex]} {year}
+            </span>
+
+            <div className="level-2 grid grid-cols-7 overflow-hidden border border-neutral-400/50">
+                <WeekdayNames />
+                {cells.map((cell) => {
+                    const positionInUserRange = getPositionInRange(cell.dateString, userRange.startDate, userRange.endDate);
+                    const blockedRange = blockedRanges?.find((r) => isInRange(cell.dateString, r.startDate, r.endDate)); // TODO could pre-filter per-month? compare cell[0] and cell[length-1] datestrings?
+                    const positionInBlockedRange = blockedRange ? getPositionInRange(cell.dateString, blockedRange.startDate, blockedRange.endDate) : null;
+                    // const isThisRangeDescription = rangeDescription === range?.name;
+
+                    return (
+                        <DayCell
+                            key={cell.dateString}
+                            dayCell={cell}
+                            positionInUserRange={positionInUserRange}
+                            positionInBlockedRange={positionInBlockedRange}
+                        />
+                    );
+                })}
+            </div>
+
+            {debug && blockedRanges && (
+                <ol className="text-2xs">
+                    {blockedRanges.map(({ startDate, endDate, description }, idx) => (
+                        <ul key={startDate + endDate + description + idx} className="mt-2">
+                            <li>{description}:</li>
+                            <li>
+                                {startDate} - {endDate}
+                            </li>
+                        </ul>
+                    ))}
+                </ol>
+            )}
+        </div>
+    );
+};
+
+const DayCell = ({
+    dayCell,
+    positionInUserRange,
+    positionInBlockedRange,
+}: {
+    dayCell: DayCellData;
+    positionInUserRange: RangePosition;
+    positionInBlockedRange: RangePosition;
+}) => {
+    const { date, monthPosition } = dayCell;
+    const isOutsideUserRangeInsideBlockedRange = !positionInUserRange && positionInBlockedRange;
+    const isAtUserRangeExtents = positionInUserRange === 'first' || positionInUserRange === 'last';
+
+    return (
+        <div
+            className={classNames(
+                '[--month-view-has-outer-left-edge:0] [--month-view-has-outer-right-edge:0] [--month-view-top-is-inner-horiz-edge:0] nth-[7n]:[--month-view-has-outer-right-edge:1] nth-[7n+8]:[--month-view-has-outer-left-edge:1] nth-[n+15]:[--month-view-top-is-inner-horiz-edge:1]',
+                'relative flex h-(--calendar-grid-cell-height) w-(--calendar-grid-cell-width) flex-col items-center justify-center',
+                monthPosition === 'currentMonth' ? 'bg-white' : 'bg-neutral-100',
+            )}
+        >
+            {/* Grid */}
+            <div className="absolute top-0 left-0 box-content size-full border-t-[calc(theme(spacing.px)*var(--month-view-top-is-inner-horiz-edge))] border-l-[calc(theme(spacing.px)*(1-var(--month-view-has-outer-left-edge)))] border-neutral-100" />
+
+            {/* Blocked-Range Cell */}
+            {positionInBlockedRange && (
+                <div
+                    className={classNames(
+                        'absolute top-1.5 right-[calc(--spacing(1)*var(--month-view-has-outer-right-edge))] bottom-1.5 left-[calc(--spacing(1)*var(--month-view-has-outer-left-edge))] rounded-l-[calc(4px*var(--month-view-has-outer-left-edge))] rounded-r-[calc(4px*var(--month-view-has-outer-right-edge))]',
+                        monthPosition === 'currentMonth'
+                            ? positionInUserRange
+                                ? positionInUserRange === 'first'
+                                    ? 'bg-linear-to-r from-red-100 to-red-400'
+                                    : positionInUserRange === 'last'
+                                      ? 'bg-linear-to-l from-red-100 to-red-400'
+                                      : // 'middle' or 'single'
+                                        'bg-red-400'
+                                : 'bg-red-100'
+                            : 'box-border border-t border-r-[calc(1px*var(--month-view-has-outer-right-edge))] border-b border-l-[calc(1px*var(--month-view-has-outer-left-edge))] border-red-300',
+                        positionInBlockedRange === 'first'
+                            ? '[--month-view-has-outer-left-edge:1]'
+                            : positionInBlockedRange === 'last'
+                              ? '[--month-view-has-outer-right-edge:1]'
+                              : positionInBlockedRange === 'single'
+                                ? '[--month-view-has-outer-left-edge:1] [--month-view-has-outer-right-edge:1]'
+                                : '',
+                    )}
+                />
+            )}
+
+            {/* User-Range start- and endpoints */}
+            <div
+                className={classNames(
+                    'absolute',
+                    isAtUserRangeExtents
+                        ? 'border-theme-cta-foreground bg-theme-cta-background top-[-5%] left-1/2 z-10 aspect-square h-[110%] -translate-x-1/2 rounded-full border-2 outline-4 outline-white'
+                        : '',
+                )}
+            />
+
+            <span
+                className={classNames(
+                    'z-20 font-light',
+                    monthPosition === 'currentMonth'
+                        ? isOutsideUserRangeInsideBlockedRange
+                            ? 'text-red-300'
+                            : isAtUserRangeExtents
+                              ? 'text-theme-cta-foreground text-base! font-normal!'
+                              : positionInBlockedRange
+                                ? 'text-red-600'
+                                : 'text-green-600'
+                        : 'text-neutral-400',
+                )}
+            >
+                {date}
+            </span>
+        </div>
+    );
+
+    function handleClick(range: DateRange | undefined) {
+        store_setRangeDescription(range?.description ? range.description : '');
+    }
+};
+
+const WeekdayNames = () =>
+    WEEKDAY_NAMES.map((weekDayName, idx) => (
+        <div
+            key={weekDayName + idx}
+            className="text-2xs w-(--calendar-grid-cell-width) border-b border-neutral-300 bg-neutral-200 pt-0.5 pb-1 text-center text-neutral-400 uppercase first-letter:text-xs"
+        >
+            {weekDayName}
+        </div>
+    ));
+
+function getPositionInRange(date: string, rangeStart: string, rangeEnd: string): RangePosition {
+    if (date < rangeStart || date > rangeEnd) return null;
+    return date === rangeStart && date === rangeEnd ? 'single' : date === rangeStart ? 'first' : date === rangeEnd ? 'last' : 'middle';
+}
+
+type RangePosition = 'first' | 'last' | 'middle' | 'single' | null;
