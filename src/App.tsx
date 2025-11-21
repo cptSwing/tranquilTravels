3ce7ca10dfc9515cb1bbbd56fe1dbdf869db8923
cptSwing <jens@jbrandenburg.de>
2025-11-21 01:31:50 +0100

    We now have versioning, and todo & thank-you mentions

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,8 @@ const App = () => (
         <Header />
 
         <main className="mx-auto flex w-(--content-width) flex-col items-center justify-start gap-y-2 py-2 [--main-elements-padding:--spacing(3)]">
+            <Todo />
+
             <Options />
             <RangeDescription />
             <Calendar />
@@ -21,7 +23,40 @@ const App = () => (
                     <Debug />
                 </div>
             )}
+
+            <Thanks />
         </main>
     </QueryClientProvider>
 );
 export default App;
+
+const Todo = () => {
+    return (
+        <div className="mt-1 mb-3 text-neutral-500">
+            <h5 className="mb-1 italic">Todo:</h5>
+            <ul className="grid grid-cols-2 gap-x-2 gap-y-0.5 pl-5 text-xs">
+                <li className="list-disc">Fix dropdown (&quot;Choose Countries&quot;) only working after 1,2 clicks</li>
+                <li className="list-disc">Make &quot;Choose Holiday Type&quot; actually do anything</li>
+                <li className="list-disc">Fix Option&apos;s elements not growing on large desktop sizes</li>
+                <li className="list-disc">Add England, Russia, US, Canada (find another API, fun)</li>
+                <li className="list-disc">Finish up &apos;thank-you&apos; list</li>
+                <li className="list-disc">Beat down any re-renders on pre-existing calendar ranges, the eternal WIP</li>
+            </ul>
+        </div>
+    );
+};
+
+const Thanks = () => {
+    return (
+        <div className="my-2 flex flex-col items-center justify-start text-neutral-400">
+            <h6 className="mb-2 italic">Thanks to</h6>
+            <div className="grid grid-cols-2 gap-x-2 gap-y-1 text-xs">
+                <div className="list-disc">OpenHolidays API</div>
+                <div className="list-disc">headlessui</div>
+                <div className="list-disc">heroicons</div>
+                <div className="list-disc">zustand</div>
+                <div className="list-disc">openapi-react-query</div>
+            </div>
+        </div>
+    );
+};
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,19 +1,16 @@
+import config from '../config/config.json';
+
 const Header = () => {
     return (
         <header className="bg-theme-accent text-theme-text-light sticky top-0 z-50 h-(--header-height) w-full shadow-xl">
-            <div className="relative mx-auto flex h-full w-(--content-width) items-center justify-center">
-                <h2 className="absolute left-0 select-none">Tranquil Travels</h2>
+            <div className="relative mx-auto flex h-full w-(--content-width) items-center justify-end pr-2 md:justify-between">
+                <div className=" ">
+                    <h2 className="leading-none tracking-tight italic">Tranquil Travels</h2>
+                    <span className="text-theme-cta-background -ml-0.5 text-sm leading-none tracking-tight">Find Silence Between the Seasons</span>
+                </div>
 
-                <div className="flex items-center justify-start gap-x-4">
-                    <a href="#...">
-                        <h5>Item 1</h5>
-                    </a>
-                    <a href="#...">
-                        <h5>Item 2</h5>
-                    </a>
-                    <a href="#...">
-                        <h5>Item 3</h5>
-                    </a>
+                <div className="translate-y-full">
+                    <span className="text-xs text-emerald-400">v. {config.version}</span>
                 </div>
             </div>
         </header>
