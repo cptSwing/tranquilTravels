014ffbfe9553e351d86f992e5a8db545760ad848
cptSwing <jens@jbrandenburg.de>
2025-11-24 15:47:42 +0100

    Gradient is randomly selected at start, Added further outline to Header, some refactoring in hook

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,27 +4,29 @@ import ChevronDownIconSVG from '../assets/chevron-down-icon-solid-24.svg?react';
 import useChangeBackground from '../hooks/useChangeBackground';
 
 const Header = () => {
-    const { changeGradient, name } = useChangeBackground();
+    const { nextGradient, name } = useChangeBackground();
 
     return (
-        <header className="text-theme-text-light sticky top-2 z-50 h-(--header-height) w-full border-t-(length:--header-border-width) border-b-(length:--header-border-width) border-[--alpha(var(--html-element-gradient-to)/80%)] bg-(--html-element-gradient-from) pr-2 shadow [--header-border-width:7px] before:absolute before:top-0 before:right-0 before:bottom-0 before:left-0 before:-z-10 before:border-t-(length:--header-border-width) before:border-b-(length:--header-border-width) before:border-neutral-400/40 before:bg-white/60">
+        <header className="sticky top-2 z-50 h-(--header-height) w-full border-t-(length:--header-border-width) border-b-(length:--header-border-width) border-[--alpha(var(--html-element-gradient-from)/80%)] bg-(--html-element-gradient-to) shadow outline outline-[--alpha(var(--html-element-gradient-from)/100%)] [--header-border-width:7px] before:absolute before:top-0 before:right-0 before:bottom-0 before:left-0 before:-z-10 before:border-t-(length:--header-border-width) before:border-b-(length:--header-border-width) before:border-neutral-500/40 before:bg-white/60">
             <div className="mx-auto flex h-[calc(var(--header-height)-(2*var(--header-border-width)))] w-(--content-width) items-center justify-between py-1 mix-blend-difference contrast-150">
                 <div className="flex h-[calc(var(--header-height)-(4*var(--header-border-width)))] items-center gap-x-2 py-1 select-none">
-                    <DeckChairSVG className="aspect-square h-full stroke-[0.75] text-neutral-400" />
+                    <DeckChairSVG className="aspect-square h-full stroke-[0.75] text-neutral-300" />
 
                     <div className="flex h-full flex-col items-start justify-center gap-y-1">
-                        <h2 className="leading-none tracking-tighter text-neutral-500 italic">Tranquil Travels</h2>
-                        <div className="ml-0.5 text-xs leading-none text-neutral-600">Find Silence Between the Seasons</div>
+                        <h2 className="leading-none tracking-tighter text-(--html-element-gradient-via,var(--color-neutral-400)) italic text-shadow-[color:--alpha(var(--html-element-gradient-via,var(--color-neutral-400))/30%)] text-shadow-sm">
+                            Tranquil Travels
+                        </h2>
+                        <div className="ml-0.5 text-xs leading-none text-neutral-500">Find Silence Between the Seasons</div>
                     </div>
                 </div>
 
                 <div className="flex h-[calc(var(--header-height)-(4*var(--header-border-width)))] flex-col items-end justify-around">
-                    <div className="flex items-center justify-end gap-x-1.5">
-                        <span className="pb-px text-sm leading-none text-neutral-500 capitalize italic">{name}</span>
-                        <button className="group cursor-pointer rounded-sm bg-neutral-700 p-px hover:bg-neutral-500" onClick={() => changeGradient()}>
-                            <ChevronDownIconSVG className="text-theme-text-light/50 group-hover:text-theme-text-dark size-4 -rotate-90" />
-                        </button>
-                    </div>
+                    <button className="group flex cursor-pointer items-center justify-end gap-x-1.5 select-none" onClick={nextGradient}>
+                        <span className="pb-px text-sm leading-none text-neutral-500 capitalize italic group-hover:text-neutral-600">{name}</span>
+                        <div className="rounded-sm bg-neutral-700 p-px group-hover:bg-neutral-600">
+                            <ChevronDownIconSVG className="size-4 -rotate-90 text-neutral-300 group-hover:text-neutral-800" />
+                        </div>
+                    </button>
                     <span className="inline-block w-full text-right text-xs leading-none text-neutral-700">v. {config.version}</span>
                 </div>
             </div>
@@ -33,5 +35,3 @@ const Header = () => {
 };
 
 export default Header;
-
-// --alpha(var(--color-lime-300) / 50%)
--- src/hooks/useChangeBackground.ts
+++ src/hooks/useChangeBackground.ts
@@ -3,48 +3,39 @@ import { useCallback, useEffect, useRef, useState } from 'react';
 import { wrapNumber } from '../lib/modulo';
 
 /* from https://gradient.page/ui-gradients */
-const backgroundGradients = [
-    { name: 'horizon', from: '#003973', to: '#E5E5BE' },
-    { name: 'bora bora', from: '#2BC0E4', to: '#EAECC6' },
-    { name: 'between the clouds', from: '#73C8A9', to: '#373B44' },
-    { name: 'sweet morning', from: '#FF5F6D', to: '#FFC371' },
-    { name: 'sunset', from: '#0B486B', to: '#F56217' },
-    { name: 'sun on the horizon', from: '#fceabb', to: '#f8b500' },
-    { name: "ed's sunset gradient", from: '#ff7e5f', to: '#feb47b' },
-    { name: 'maldives', from: '#B2FEFA', to: '#0ED2F7' },
-    { name: 'velvet sun', from: '#e1eec3', to: '#f05053' },
-    { name: 'evening night', from: '#005AA7', to: '#FFFDE4' },
-    { name: 'cool sky', from: '#2980B9', via: '#6DD5FA', to: '#FFFFFF' },
-    { name: 'evening sunshine', from: '#b92b27', to: '#1565C0' },
-    { name: 'true sunset', from: '#fa709a', to: '#fee140' },
-    { name: 'sleepness night', from: '#5271C4', via: '#B19FFF', to: '#ECA1FE' },
-    { name: 'supreme sky', from: '#D4FFEC', via: '#57F2CC', to: '#4596FB' },
-    { name: 'noon to dusk', from: '#ff6e7f', to: '#bfe9ff' },
-    { name: 'sky', from: '#076585', to: '#FFFFFF' },
-    { name: 'the sky and the sea', from: '#F7941E', to: '#004E8F' },
-    { name: 'ibiza sunset', from: '#ee0979', to: '#ff6a00' },
+const backgroundGradients: { name: string; from: string; to: string; via?: string }[] = [
+    { name: 'horizon', to: '#003973', from: '#E5E5BE' },
+    { name: 'bora bora', to: '#2BC0E4', from: '#EAECC6' },
+    { name: 'between the clouds', to: '#73C8A9', from: '#373B44' },
+    { name: 'sweet morning', to: '#FF5F6D', from: '#FFC371' },
+    { name: 'sunset', to: '#0B486B', from: '#F56217' },
+    { name: 'sun on the horizon', to: '#fceabb', from: '#f8b500' },
+    { name: "ed's sunset gradient", to: '#ff7e5f', from: '#feb47b' },
+    { name: 'maldives', to: '#B2FEFA', from: '#0ED2F7' },
+    { name: 'velvet sun', to: '#e1eec3', from: '#f05053' },
+    { name: 'evening night', to: '#005AA7', from: '#FFFDE4' },
+    { name: 'cool sky', to: '#2980B9', via: '#6DD5FA', from: '#FFFFFF' },
+    { name: 'evening sunshine', to: '#b92b27', from: '#1565C0' },
+    { name: 'true sunset', to: '#fa709a', from: '#fee140' },
+    { name: 'sleepness night', to: '#5271C4', via: '#B19FFF', from: '#ECA1FE' },
+    { name: 'supreme sky', to: '#D4FFEC', via: '#57F2CC', from: '#4596FB' },
+    { name: 'noon to dusk', to: '#ff6e7f', from: '#bfe9ff' },
+    { name: 'sky', to: '#076585', from: '#FFFFFF' },
+    { name: 'the sky and the sea', to: '#F7941E', from: '#004E8F' },
+    { name: 'ibiza sunset', to: '#ee0979', from: '#ff6a00' },
 ];
+
+const randomGradientIndex = Math.floor(backgroundGradients.length * Math.random());
 
 const useChangeBackground = () => {
     const htmlElement_Ref = useRef<HTMLHtmlElement | null>(null);
-    const [currentIndex, setCurrentIndex] = useState(0);
+    const [currentIndex, setCurrentIndex] = useState(randomGradientIndex);
 
     useEffect(() => {
         if (!htmlElement_Ref.current) {
             htmlElement_Ref.current = document.querySelector('html');
             if (htmlElement_Ref.current) {
-                const properties = backgroundGradients[currentIndex].via
-                    ? {
-                          '--html-element-gradient-from': backgroundGradients[currentIndex].from,
-                          '--html-element-gradient-to': backgroundGradients[currentIndex].to,
-                          '--tw-gradient-stops': `var(--tw-gradient-position), var(--tw-gradient-from) var(--tw-gradient-from-position), ${backgroundGradients[currentIndex].via} var(--tw-gradient-via-position), var(--tw-gradient-to) var(--tw-gradient-to-position)`,
-                      }
-                    : {
-                          '--html-element-gradient-from': backgroundGradients[currentIndex].from,
-                          '--html-element-gradient-to': backgroundGradients[currentIndex].to,
-                      };
-
-                setCssProperties(htmlElement_Ref.current, properties);
+                setGradientVariables(htmlElement_Ref.current, currentIndex);
             }
         }
     });
@@ -52,25 +43,37 @@ const useChangeBackground = () => {
     const next_Cb = useCallback(() => {
         if (htmlElement_Ref.current) {
             const nextIndex = wrapNumber(currentIndex + 1, backgroundGradients.length);
-
-            const properties = backgroundGradients[nextIndex].via
-                ? {
-                      '--html-element-gradient-from': backgroundGradients[nextIndex].from,
-                      '--html-element-gradient-to': backgroundGradients[nextIndex].to,
-                      '--tw-gradient-stops': `var(--tw-gradient-position), var(--tw-gradient-from) var(--tw-gradient-from-position), ${backgroundGradients[nextIndex].via} var(--tw-gradient-via-position), var(--tw-gradient-to) var(--tw-gradient-to-position)`,
-                  }
-                : {
-                      '--html-element-gradient-from': backgroundGradients[nextIndex].from,
-                      '--html-element-gradient-to': backgroundGradients[nextIndex].to,
-                  };
-
-            setCssProperties(htmlElement_Ref.current, properties);
-
+            setGradientVariables(htmlElement_Ref.current, nextIndex);
             setCurrentIndex(nextIndex);
         }
     }, [currentIndex, setCurrentIndex]);
 
-    return { changeGradient: next_Cb, name: backgroundGradients[currentIndex].name };
+    return { nextGradient: next_Cb, name: backgroundGradients[currentIndex].name };
 };
 
 export default useChangeBackground;
+
+function setGradientVariables(element: HTMLElement, gradientIndex: number) {
+    const properties = backgroundGradients[gradientIndex].via
+        ? {
+              '--html-element-gradient-from': backgroundGradients[gradientIndex].from,
+              '--html-element-gradient-to': backgroundGradients[gradientIndex].to,
+              '--html-element-gradient-via': backgroundGradients[gradientIndex].via,
+
+              '--tw-gradient-stops':
+                  'var(--tw-gradient-position), var(--tw-gradient-from) var(--tw-gradient-from-position), var(--html-element-gradient-via) var(--tw-gradient-via-position), var(--tw-gradient-to) var(--tw-gradient-to-position)',
+              // NOTE ^^^ This is the only way to truly have a gradient's middle value be used conditionally in tailwind. If via- is assigned in html as tailwind class, it will always default to 'white' when set to undefined (since it's necessarily defined as '#fff' in @property declaration). Not too happy with this, but here we are.
+          }
+        : {
+              '--html-element-gradient-from': backgroundGradients[gradientIndex].from,
+              '--html-element-gradient-to': backgroundGradients[gradientIndex].to,
+              '--html-element-gradient-via': '',
+
+              // NOTE (see above note)
+              '--tw-gradient-stops':
+                  'var(--tw-gradient-via-stops, var(--tw-gradient-position), var(--tw-gradient-from) var(--tw-gradient-from-position), var(--tw-gradient-to) var(--tw-gradient-to-position))',
+          };
+
+    // @ts-expect-error 'function paramater type needs to be a little less strict'
+    setCssProperties(element, properties);
+}
