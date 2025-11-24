aca14cf5fd5271b8d04ee56779a0795ebf0c69ee
cptSwing <jens@jbrandenburg.de>
2025-11-24 15:48:23 +0100

    Thanks moved to separate component, todos added at repo's issues

--- src/App.tsx
+++ src/App.tsx
@@ -6,14 +6,13 @@ import RangeDescription from './components/RangeDescription';
 import Options from './components/Options';
 import Debug from './components/Debug';
 import config from './config/config.json';
+import Acknowledgements from './components/Acknowledgements';
 
 const App = () => (
     <QueryClientProvider client={queryClientTanstack}>
         <Header />
 
-        <main className="mx-auto flex w-(--content-width) flex-col items-center justify-start gap-y-2 py-2 [--main-elements-padding:--spacing(3)]">
-            <Todo />
-
+        <main className="mx-auto mt-6 mb-4 flex w-(--content-width) flex-col items-center justify-start gap-y-2 [--main-elements-padding:--spacing(3)]">
             <Options />
             <RangeDescription />
             <Calendar />
@@ -24,47 +23,8 @@ const App = () => (
                 </div>
             )}
 
-            <Thanks />
+            <Acknowledgements />
         </main>
     </QueryClientProvider>
 );
 export default App;
-
-const Todo = () => {
-    return (
-        <div className="mt-1 mb-3 text-neutral-500">
-            <h5 className="mb-1 italic">Todo:</h5>
-            <ul className="grid grid-cols-2 gap-x-2 gap-y-0.5 pl-5 text-xs">
-                <li className="list-disc">Fix dropdown (&quot;Choose Countries&quot;) only working after 1,2 clicks</li>
-                <li className="list-disc">Make &quot;Choose Holiday Type&quot; actually do anything</li>
-                <li className="list-disc">Cursor-pointer; Text fixes (&apos;Assignees&apos; etc)</li>
-                <li className="list-disc">Fix Option&apos;s elements not growing on large desktop sizes</li>
-                <li className="list-disc">Add England, Russia, US, Canada (find another API, fun)</li>
-                <li className="list-disc">Finish up &apos;thank-you&apos; list</li>
-                <li className="list-disc">Beat down any re-renders on pre-existing calendar ranges, the eternal WIP</li>
-            </ul>
-        </div>
-    );
-};
-
-const Thanks = () => {
-    return (
-        <div className="my-2 flex flex-col items-center justify-start text-neutral-400">
-            <h6 className="mb-2 italic">Thanks to</h6>
-            <div className="grid grid-cols-2 gap-x-2 gap-y-1 text-xs">
-                <div className="list-disc">OpenHolidays API</div>
-                <div className="list-disc">headlessui</div>
-                <div className="list-disc">heroicons</div>
-                <div className="list-disc">zustand</div>
-                <div className="list-disc">
-                    fredley (eck chair from{' '}
-                    <a href="https://thenounproject.com/browse/icons/term/deck-chair/" target="_blank" rel="noreferrer">
-                        Noun Project
-                    </a>
-                    )
-                </div>
-                <div className="list-disc">openapi-react-query</div>
-            </div>
-        </div>
-    );
-};
---
+++ src/components/Acknowledgements.tsx
@@ -0,0 +1,25 @@
+const Acknowledgements = () => {
+    return (
+        <div className="flex flex-col items-center justify-start text-neutral-400">
+            <h6 className="mb-2 italic">Thanks to</h6>
+            <div className="grid grid-cols-2 gap-x-2 gap-y-1 text-xs">
+                <div className="list-disc">OpenHolidays API</div>
+                <div className="list-disc">headlessui</div>
+                <div className="list-disc">heroicons</div>
+                <div className="list-disc">
+                    <a href="https://gradient.page/ui-gradients" target="_blank" rel="noreferrer">
+                        Gradient Page
+                    </a>
+                </div>
+                <div className="list-disc">
+                    <a href="https://thenounproject.com/browse/icons/term/deck-chair/" target="_blank" rel="noreferrer">
+                        fredley&apos;s deck chair icon (Noun Project)
+                    </a>
+                </div>
+                <div className="list-disc">openapi-react-query</div>
+            </div>
+        </div>
+    );
+};
+
+export default Acknowledgements;
