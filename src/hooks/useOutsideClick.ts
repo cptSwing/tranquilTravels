eac6359ab571082580834c4dc150be2760a262d7
cptSwing <jens@jbrandenburg.de>
2025-11-25 13:24:57 +0100

    adding useOutsideClick hook

---
+++ src/hooks/useOutsideClick.ts
@@ -0,0 +1,23 @@
+import { useEffect, useRef } from 'react';
+
+const useOutsideClick = (callback: () => void) => {
+    const ref = useRef<HTMLElement | null>(null);
+
+    useEffect(() => {
+        const handleClick = (event: MouseEvent) => {
+            if (ref.current && !ref.current.contains(event.target as Node)) {
+                callback();
+            }
+        };
+
+        document.addEventListener('click', handleClick, { capture: true });
+
+        return () => {
+            document.removeEventListener('click', handleClick, { capture: true });
+        };
+    }, [ref, callback]);
+
+    return ref;
+};
+
+export default useOutsideClick;
