7013bbbfcf29216415193427488a61ecd43559e3
cptSwing <jens@jbrandenburg.de>
2025-11-26 02:29:05 +0100

    got rid of <Debug>

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,20 +1,12 @@
 import Calendar from './Calendar';
 import Options from './Options';
-import Debug from './Debug';
-import config from '../config/config.json';
 import Acknowledgements from './Acknowledgements';
 
 const Main = () => {
     return (
-        <main className="mx-auto mt-6 mb-4 flex w-(--content-width) flex-col items-center justify-start gap-y-2 [--main-elements-padding:--spacing(3)]">
+        <main className="mx-auto mt-6 mb-4 flex w-(--content-width) flex-col items-center justify-start gap-y-4 [--main-elements-padding:--spacing(3)]">
             <Options />
             <Calendar />
-
-            {config.debug && (
-                <div className="bg-neutral-500 p-2">
-                    <Debug />
-                </div>
-            )}
 
             <Acknowledgements />
         </main>
